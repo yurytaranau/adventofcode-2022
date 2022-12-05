ec9178eb4b0471f4b31b52491718146f6edd99af
Yury Taranau <yury.taranau@gmail.com>
2022-12-05 18:26:44 +0100

    Added day5

---
+++ day5.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type stack []string
+
+func (s stack) Push(v string) stack {
+	return append(s, v)
+}
+
+func (s stack) Pop() (stack, string) {
+	l := len(s)
+	return s[:l-1], s[l-1]
+}
+
+func (s stack) Reverse() stack {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
+
+func (d days) Day5() {
+	// Part1
+	file, err := os.Open("sources/day5")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fs := bufio.NewScanner(file)
+	fs.Split(bufio.ScanLines)
+
+	parseCrates := func(line string, stacks []stack) []stack {
+		parseCrateName := func(s []string) string {
+			r := []rune(s[1])
+			if r[0] >= 65 && r[0] <= 90 {
+				return string(r[0])
+			}
+			return ""
+		}
+
+		crate := make([]string, 0, 3)
+		i := 0
+		crateCompleted := false
+		for _, r := range strings.Split(line, "") {
+			if crateCompleted {
+				//skip empty spaces beween crates
+				crateCompleted = false
+				continue
+			}
+			crate = append(crate, r)
+			if len(crate) == 3 {
+				// analyze crate name
+				crateName := parseCrateName(crate)
+				if crateName != "" {
+					stacks[i] = stacks[i].Push(crateName)
+				}
+				i += 1
+				crate = make([]string, 0, 3)
+				crateCompleted = true
+			}
+
+		}
+		return stacks
+	}
+
+	moveCrate := func(stacks []stack, from, to int) []stack {
+		stack, item := stacks[from].Pop()
+		stacks[from] = stack
+		stacks[to] = stacks[to].Push(item)
+		return stacks
+	}
+
+	stacks := make([]stack, 9)
+	stacksCompleted := false
+	for fs.Scan() {
+		// parse stacks of crates
+		if fs.Text() != "" && !stacksCompleted {
+			stacks = parseCrates(fs.Text(), stacks)
+			continue
+		} else if fs.Text() == "" {
+			stacksCompleted = true
+			// invert stacks
+			for i, s := range stacks {
+				stacks[i] = s.Reverse()
+			}
+		}
+		// parse and execute movements
+		re := regexp.MustCompile("[0-9]+")
+		command := re.FindAllString(fs.Text(), -1)
+		if len(command) > 0 {
+			iterate, err := strconv.Atoi(command[0])
+			if err != nil {
+				panic("something went wrong")
+			}
+			for {
+				if iterate == 0 {
+					break
+				}
+				from, err := strconv.Atoi(command[1])
+				if err != nil {
+					panic("something went wrong")
+				}
+				to, err := strconv.Atoi(command[2])
+				if err != nil {
+					panic("something went wrong")
+				}
+				stacks = moveCrate(stacks, from-1, to-1)
+				iterate--
+			}
+		}
+
+	}
+	// dump stack heads
+	var heads string
+	for _, s := range stacks {
+		_, head := s.Pop()
+		heads += head
+	}
+	fmt.Printf("Stacks head (part 1): %s\n", heads)
+
+	// Part2
+	file, err = os.Open("sources/day5")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fs = bufio.NewScanner(file)
+	fs.Split(bufio.ScanLines)
+
+	moveCrates := func(stacks []stack, count, from, to int) []stack {
+		crates := stack{}
+		// extract required crates
+		i := count
+		for {
+			if i == 0 {
+				break
+			}
+			stack, item := stacks[from].Pop()
+			stacks[from] = stack
+			crates = crates.Push(item)
+			i--
+		}
+		// push to new stack
+		i = count
+		for {
+			if i == 0 {
+				break
+			}
+			stack, item := crates.Pop()
+			crates = stack
+			stacks[to] = stacks[to].Push(item)
+			i--
+		}
+		return stacks
+	}
+
+	stacks = make([]stack, 9)
+	stacksCompleted = false
+	for fs.Scan() {
+		// parse stacks of crates
+		if fs.Text() != "" && !stacksCompleted {
+			stacks = parseCrates(fs.Text(), stacks)
+			continue
+		} else if fs.Text() == "" {
+			stacksCompleted = true
+			// invert stacks
+			for i, s := range stacks {
+				stacks[i] = s.Reverse()
+			}
+		}
+		// parse and execute movements
+		re := regexp.MustCompile("[0-9]+")
+		command := re.FindAllString(fs.Text(), -1)
+		if len(command) > 0 {
+			count, err := strconv.Atoi(command[0])
+			if err != nil {
+				panic("something went wrong")
+			}
+			from, err := strconv.Atoi(command[1])
+			if err != nil {
+				panic("something went wrong")
+			}
+			to, err := strconv.Atoi(command[2])
+			if err != nil {
+				panic("something went wrong")
+			}
+			stacks = moveCrates(stacks, count, from-1, to-1)
+		}
+
+	}
+	// dump stack heads
+	heads = ""
+	for _, s := range stacks {
+		_, head := s.Pop()
+		heads += head
+	}
+	fmt.Printf("Stacks head (part 2): %s\n", heads)
+}
